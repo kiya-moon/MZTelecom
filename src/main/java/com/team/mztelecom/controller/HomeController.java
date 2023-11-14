3a22e01126f3ef13645af2d8116a0fa3769110f2
KimSiWoo <gkgkdnaos@gmail.com>
2023-11-14 17:26:46 +0900

    Test : 레이아웃 설정 Feat : logging 설정

--- src/main/java/com/team/mztelecom/controller/HomeController.java
+++ src/main/java/com/team/mztelecom/controller/HomeController.java
@@ -7,19 +7,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
 	
-	@GetMapping("/")
-    public String getBoardIndexPage(Locale locale, Model model){
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	public String goHome(HttpServletRequest request) {
 		
 		logger.info("테스트해본다");
+		logger.debug("테스트해본다");
 		
-        return "templates/contentsTest";
-    }
+		return "content/home";
+	}
 }
