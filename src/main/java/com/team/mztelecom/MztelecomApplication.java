64d9117dc03ab26531646877b1b2a2c704eff60c
KimSiWoo <gkgkdnaos@gmail.com>
2023-11-14 15:21:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/team/mztelecom/MztelecomApplication.java
@@ -0,0 +1,14 @@
+package com.team.mztelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class MztelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MztelecomApplication.class, args);
+	}
+
+}
