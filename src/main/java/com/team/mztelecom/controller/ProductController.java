a0d7d321a1c06f89a22cdd2cb641909dd4788c0f
parkJi-yun <nmjk0123@naver.com>
2023-11-26 13:11:53 +0900

    Refactor: 통합검색 삭제

--- src/main/java/com/team/mztelecom/controller/ProductController.java
+++ src/main/java/com/team/mztelecom/controller/ProductController.java
@@ -1,36 +1,5 @@
 package com.team.mztelecom.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.team.mztelecom.domain.Product;
-import com.team.mztelecom.service.ProductService;
-
-
-@RestController
 public class ProductController {
-	
-	@Autowired
-	ProductService productService;
-	
-	public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-	
-	@GetMapping("/search.do")
-    public List<Product> searchProducts(@RequestParam("keyword") String keyword) {
-        List<Product> products;
-        if (keyword != null && !keyword.isEmpty()) {
-            products = productService.searchProductsByName(keyword);
-        } else {
-            products = productService.getAllProducts();
-        }
-        return products;
-    }
+
 }
