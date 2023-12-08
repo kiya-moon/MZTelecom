7357986fcde9f1b968c1db0d93ee048de896f4de
parkJi-yun <nmjk0123@naver.com>
2023-12-08 16:31:54 +0900

    Feat: id 기억하기

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -58,6 +58,25 @@ function clearInput(inputId) {
 }
 
 function togglePassword() {
-    var passwordField = document.getElementById('password');
+    const passwordField = document.getElementById('password');
     passwordField.type = (passwordField.type === "password") ? "text" : "password";
+}
+
+// id 기억하기
+function saveUsername() {
+    const rememberCheckbox = document.getElementById('remember');
+    const usernameInput = document.getElementById('username');
+
+    if (rememberCheckbox.checked) {
+        localStorage.setItem('savedUsername', usernameInput.value);
+    } else {
+        localStorage.removeItem('savedUsername');
+    }
+    
+}
+
+
+if(localStorage.getItem('savedUsername')){
+	document.getElementById('username').value = localStorage.getItem('savedUsername');
+	document.getElementById('remember').checked = true;
 }
