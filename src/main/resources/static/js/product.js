ab578254b443b31799468b7b0163a3bf6eff3a33
parkJi-yun <nmjk0123@naver.com>
2023-12-14 05:03:27 +0900

    Refactor: 찜하기 data

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -16,7 +16,7 @@ function handleLikeClick(event, productId, liked) {
 		return;
 	}
 
-	fetch('/product/' + productId + '/like', {
+	fetch('/product/' + productId + '/liked', {
 		method: 'PUT',
 		headers: {
 			[csrfHeader]: csrfToken,
@@ -27,15 +27,17 @@ function handleLikeClick(event, productId, liked) {
 			if (!response.ok) {
 				toggleHeartColor(button); // 실패 시 다시 원래 색상으로 되돌림
 			}
+			
 			return response.json(); // 여기서 서버 응답을 JSON 형태로 파싱
 		})
 		.then(data => {
-			const isLoggedIn = data.isLoggedIn; // 서버에서 전달한 로그인 상태 필드
+			const isLoggedIn = data; // 서버에서 전달한 로그인 상태 필드
 			// 이후 작업: 로그인 상태에 따른 동작 수행
+			
 			if (!isLoggedIn) {
 				alert('로그인이 필요합니다!');
 				toggleHeartColor(button);
-			}
+			} 
 		})
 		.catch(error => {
 			console.error('Error:', error);
