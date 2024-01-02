1ac54d8eee3d84b215e7f1187108950e6ef436e6
parkJi-yun <nmjk0123@naver.com>
2024-01-03 03:05:46 +0900

    Refactor: login.js -> signup.js 체크박스 전체 선택 기능

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -85,3 +85,32 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 });
 
+/* signup checkbox */
+const signupForm = document.getElementById('signup-form');
+const agreeAllCheckbox = document.getElementById('agreeAll');
+const otherCheckboxes = document.querySelectorAll('.signup-checkbox input[type="checkbox"]');
+
+// 다른 체크박스들의 클릭 이벤트 모니터링
+otherCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', function() {
+        // 모든 체크박스가 선택되었는지 확인
+        const allChecked = Array.from(otherCheckboxes).every(checkbox => checkbox.checked);
+
+        // 전체 동의 체크박스 상태 업데이트
+        agreeAllCheckbox.checked = allChecked;
+    });
+});
+
+// 전체 동의 체크박스의 클릭 이벤트 처리
+agreeAllCheckbox.addEventListener('click', function() {
+    const isChecked = agreeAllCheckbox.checked;
+    otherCheckboxes.forEach(checkbox => checkbox.checked = isChecked);
+});
+
+
+signupForm.addEventListener('submit', function(e) {
+	if (!agreeAllCheckbox.checked) {
+		e.preventDefault();
+		alert('체크 박스를 모두 체크해주세요.');
+	}
+});
