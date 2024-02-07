bb98e2551cb4cc47a0777f4ffc0b48995a5c8d67
KimSiWoo <gkgkdnaos@gmail.com>
2024-02-07 15:45:23 +0900

    Refactor : 마이페이지 주문내역 수정

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -1,51 +1,6 @@
 const csrfToken = document.querySelector("meta[name='_csrf']").getAttribute("content");
 const csrfHeader = document.querySelector("meta[name='_csrf_header']").getAttribute("content");
 
-window.onload = function(){
-	// 주문내역 페이지 기본 날짜 설정
-	const startDate = new Date();
-	const endDate = new Date();
-	
-	startDate.setMonth(endDate.getMonth() - 1);
-	
-	document.getElementById('startDate').value = startDate.toISOString().substring(0, 10);
-	document.getElementById('endDate').value = endDate.toISOString().substring(0, 10);
-	
-}
-
-// 3개월 버튼 클릭 시
-function setThreeMonth() {
-	const startDate = new Date();
-	const endDate = new Date();
-	
-	startDate.setMonth(endDate.getMonth() - 3);
-	
-	document.getElementById('startDate').value = startDate.toISOString().substring(0, 10);
-	document.getElementById('endDate').value = endDate.toISOString().substring(0, 10);
-}
-
-// 6개월 버튼 클릭 시
-function setSixMonth() {
-	const startDate = new Date();
-	const endDate = new Date();
-	
-	startDate.setMonth(endDate.getMonth() - 6);
-	
-	document.getElementById('startDate').value = startDate.toISOString().substring(0, 10);
-	document.getElementById('endDate').value = endDate.toISOString().substring(0, 10);
-}
-
-// 1년 버튼 클릭 시
-function setOneYear() {
-	const startDate = new Date();
-	const endDate = new Date();
-	
-	startDate.setFullYear(endDate.getFullYear() - 1);
-	
-	document.getElementById('startDate').value = startDate.toISOString().substring(0, 10);
-	document.getElementById('endDate').value = endDate.toISOString().substring(0, 10);
-}
-
 // 회원정보수정
 
 	// 비밀번호 확인 이벤트 발생
